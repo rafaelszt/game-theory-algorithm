dd4a179dfda7f6ff19bff8c5337ea41cc57a02ce
Rafael Teixeira <rafael.szt@gmail.com>
2017-05-14 20:56:50 -0300

    Adds super class for search

---
+++ SearchAlgorithm.h
@@ -0,0 +1,23 @@
+#ifndef GAME_THEORY_ALGORITHM_SEARCHALGORITHM_H
+#define GAME_THEORY_ALGORITHM_SEARCHALGORITHM_H
+
+#include "Field.h"
+#include <cmath>
+
+using statusCoordinate = std::pair<int, std::pair<int, int>>*;
+
+class SearchAlgorithm {
+protected:
+    std::vector<Field *> *generatePossiblePlays(Field *f, bool firstPlayer);
+
+    statusCoordinate minimumValue(statusCoordinate a, statusCoordinate b);
+    statusCoordinate maximumValue(statusCoordinate a, statusCoordinate b);
+    statusCoordinate getHeuristicValue(Field *field);
+    int getBoardPoint(int x, int y);
+
+public:
+    virtual statusCoordinate execute(Field *field, int depth, bool maximizingPlayer, bool firstPlayer) = 0;
+};
+
+
+#endif //GAME_THEORY_ALGORITHM_SEARCHALGORITHM_H
